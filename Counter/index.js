ca1488058d9cfa8e18ae250da295aaa8ae306dd3
sandranymark <sandra@nymark.nu>
2024-01-07 20:58:59 +0100

    uppdate

--- Counter/index.js
+++ Counter/index.js
@@ -13,4 +13,19 @@ function increment() {
     console.log("The button was clicked")
 }
 
+let countEl = document.getElementById("count-el")
 
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+    console.log(count)
+}
+
+
+function save() {
+    console.log(count)
+}
