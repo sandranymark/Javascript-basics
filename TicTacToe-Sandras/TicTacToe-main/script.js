ce4c0866661163480d054298b0fd44649e09346b
sandranymark <sandra@nymark.nu>
2024-01-23 17:18:18 +0100

    Etapp 2

--- TicTacToe-Sandras/TicTacToe-main/script.js
+++ TicTacToe-Sandras/TicTacToe-main/script.js
@@ -1,154 +1,137 @@
 "use strict";
 
-/**
- * Globalt objekt som innehåller de attribut som ni skall använda.
- * Initieras genom anrop till funktionern initGlobalObject().
- */
-let oGameData = {};
+let game = null;
 
-initGlobalObject();
-
-/**
- * Initerar det globala objektet med de attribut som ni skall använda er av.
- * Funktionen tar inte emot några värden.
- * Funktionen returnerar inte något värde.
- */
-function initGlobalObject() {
-
-    //Datastruktur för vilka platser som är lediga respektive har brickor
-    //Genom at fylla i här med antingen X eler O kan ni testa era rättningsfunktioner 
-    oGameData.gameField = ['', '', '', '', '', '', '', '', ''];
-
-    /* Testdata för att testa rättningslösning */
-    // oGameData.gameField = ['X', 'X', 'X', '', '', '', '', '', ''];
-    //oGameData.gameField = ['X', '', '', 'X', '', '', 'X', '', ''];
-    //oGameData.gameField = ['X', '', '', '', 'X', '', '', '', 'X'];
-    //oGameData.gameField = ['', '', 'X', '', 'X', '', 'X', '', ''];
-    //oGameData.gameField = ['X', 'O', 'X', '0', 'X', 'O', 'O', 'X', 'O'];
-
-    //Indikerar tecknet som skall användas för spelare ett.
-    oGameData.playerOne = "X";
-
-    //Indikerar tecknet som skall användas för spelare två.
-    oGameData.playerTwo = "O";
-
-    //Kan anta värdet X eller O och indikerar vilken spelare som för tillfället skall lägga sin "bricka".
-    oGameData.currentPlayer = "";
-
-    //Nickname för spelare ett som tilldelas från ett formulärelement,
-    oGameData.nickNamePlayerOne = "";
-
-    //Nickname för spelare två som tilldelas från ett formulärelement.
-    oGameData.nickNamePlayerTwo = "";
-
-    //Färg för spelare ett som tilldelas från ett formulärelement.
-    oGameData.colorPlayerOne = "";
-
-    //Färg för spelare två som tilldelas från ett formulärelement.
-    oGameData.colorPlayerTwo = "";
-
-    //Antalet sekunder för timerfunktionen
-    oGameData.seconds = 5;
-
-    //Timerns ID
-    oGameData.timerId = null;
-
-    //Från start är timern inaktiverad
-    oGameData.timerEnabled = false;
-
-    //Referens till element för felmeddelanden
-    oGameData.timeRef = document.querySelector("#errorMsg");
-}
-
-/**
- * Kontrollerar för tre i rad.
- * Returnerar 0 om det inte är någon vinnare, 
- * returnerar 1 om spelaren med ett kryss (X) är vinnare,
- * returnerar 2 om spelaren med en cirkel (O) är vinnare eller
- * returnerar 3 om det är oavgjort.
- * Funktionen tar inte emot några värden.
- */
-function checkForGameOver() {
-    //Kontrollerar om "X" vunnit genom att köra rättningsfunktionerna, om så är fallet returneras 1
-    if (checkWinner('X')) {
-        return 1;
-    }
-    //Kontrollerar om "O" vunnit genom att köra rättningsfunktionerna, om så är fallet returneras 2
-    if (checkWinner('O')) {
-        return 2;
-    }
-    //Kontrollerar om spelet är oavgjort, returnerar isåfall 3
-    if (checkForDraw()) {
-        return 3;
-    }
-    //Annars returneras 0, och spelet fortlöper
-    else {
-        return 0;
+class Player {
+    constructor(char, nickname, color) {
+        this.nickname = nickname;
+        this.color = color;
+        this.char = char;
     }
 }
 
-//Skapa en array av alla vinnande kombinationer.
-//Skapa en flagga för isWinner.
-//Loopa igenom alla winningCombos.
-//I varje loop kontrollerar ni om alla platser i oGameData.GameField 
-//som motsvarar nuvarande combo innehåller playerIn. Om sant, ändra värdet på flaggan.
-//Returnera flaggan isWinner
-function checkWinner(playerIn) {
-    const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], //Horisontella kombinationer 
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], //Vertikala kombinationer
-    [0, 4, 8], [2, 4, 6] //Diagonala kombinationer
-    ];
+class Game {
+    seconds = 5;
+    showTimer = false;
+    currentPlayer = null;
+    gameField = ['', '', '', '', '', '', '', '', '']
 
-    let isWinner = false;
+    constructor(playerOne, playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+    }
 
-    isWinner = winningCombos.some(combo => combo.every(position => oGameData.gameField[position] === playerIn));
-    return isWinner;
+    move(index) {
+        this.gameField[index] = this.currentPlayer.char;
+        console.log(index, "Making a move on the field")
+        console.log(this.gameField, "Current gamefield");
 
-}
+        if (this.isWinner(this.currentPlayer)) {
+            this.gameOver(this.currentPlayer);
+            return;
+        }
 
+        // Spelet är utan vinnare, det är oavgjort.
+        if (this.gameCompleted()) {
+            this.gameOver();
+            return;
+        }
 
+        game.switchPlayer();
 
-//Kontrollera om alla platser i oGameData.GameField är fyllda. Om sant returnera true, annars false.
-function checkForDraw() {
+    }
 
-    return oGameData.gameField.every(position => position !== '');
+    switchPlayer() {
+        this.currentPlayer = this.currentPlayer === this.playerOne ? this.playerTwo : this.playerOne;
+        document.dispatchEvent(new CustomEvent('changePlayer', { detail: { player: this.currentPlayer } }))
+    }
+
+    gameOver(winner = null) {
+
+        var result = { winner: winner }
+
+        document.dispatchEvent(new CustomEvent('gameOver', { detail: result }))
+    }
+
+    isWinner(player) {
+
+        const winningCombos = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        return winningCombos.some(combo => combo.every(position => this.gameField[position] === player.char));
+    }
+
+    gameCompleted() {
+        return this.gameField.every(position => position !== '')
+    };
 }
 
 //Funktion som förbereder spelet inför start
 function prepGame() {
-    //Hämtar in spelarnas nicknames och färger från formuläret
-    oGameData.nickNamePlayerOne = document.querySelector("#playerOne").value;
-    oGameData.nickNamePlayerTwo = document.querySelector("#playerTwo").value;
-    oGameData.colorPlayerOne = document.querySelector("#colorOne").value;
-    oGameData.colorPlayerTwo = document.querySelector("#colorTwo").value;
 
-    //Kallar på funktionen som slumpar vem som börjar
-    randomPlayer();
-
-    //Kallar på funktionen som startar spelet
-    initiateGame();
+    let startGameBtn = document.getElementById("newGame");
+    startGameBtn.addEventListener("click", initiateGame);
 }
 
-function validateForm() {
-
-}
 
 function initiateGame() {
 
-}
+    let form = document.getElementById("theForm");
+    let gameArea = document.getElementById("gameArea");
+    let jumbotronText = document.querySelector(".jumbotron h1");
 
-function executeMove() {
+    form.classList.add("d-none");
+    gameArea.classList.remove("d-none");
 
-}
+    var playerOne = new Player('X', document.getElementById("nick1").value, document.getElementById("color1").value);
+    var playerTwo = new Player('O', document.getElementById("nick2").value, document.getElementById("color2").value);
 
-function changePlayer() {
+    game = new Game(playerOne, playerTwo);
 
-}
+    // Slumpa vem som börjar
+    game.currentPlayer = Math.random() < 0.5 ? game.playerOne : game.playerTwo;
 
-function timer() {
+    jumbotronText.textContent = `Aktuell spelare är ${game.currentPlayer.nickname}`;
 
-}
+    let cells = document.querySelectorAll("#gameArea td");
 
-function gameOver() {
+    let cellClicked = function (event) {
+        event.target.innerHTML = game.currentPlayer.char;
+        let index = event.target.getAttribute('data-id');;
+        game.move(index);
+    }
+    // Sätt upp spelplanen
+    // Använder for-loop för något bättre prestanda, använd foreach för bättre läsbarhet.
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].textContent = "";
+        cells[i].addEventListener("click", cellClicked);
+    }
 
+    document.addEventListener('changePlayer', (event) => {
+        jumbotronText.textContent = `Aktuell spelare är ${event.detail.player.nickname}`;
+    });
+
+    document.addEventListener('gameOver', (event) => {
+
+        // Avregistrera alla klicklyssnare
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].removeEventListener("click", cellClicked);
+        }
+
+        let winner = event.detail.winner;
+
+        if (winner !== null) {
+            jumbotronText.textContent = `Vinnaren är ${winner.nickname}`;
+        } else {
+            jumbotronText.textContent = `Oavgjort`;
+        }
+    });
 }
